1261c3eee8efbe34ad206bde84a03f996075eeb4
Gordon Marler <gmarler@gmarler.com>
2011-09-08 23:31:37 -0400

    add http authentication

---
+++ LolCatalyst-Lite/lib/LolCatalyst/Lite/Auth/Schema.pm
@@ -0,0 +1,19 @@
+package LolCatalyst::Lite::Auth::Schema;
+
+# Created by DBIx::Class::Schema::Loader
+# DO NOT MODIFY THE FIRST PART OF THIS FILE
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Schema';
+
+__PACKAGE__->load_namespaces;
+
+
+# Created by DBIx::Class::Schema::Loader v0.07010 @ 2011-09-08 23:29:29
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:kB9QXmWGtTO2sStNaYm1vQ
+
+
+# You can replace this text with custom code or comments, and it will be preserved on regeneration
+1;
